e6c5cfd68495f0feb3c91037ce0118a2927b7ca5
Anastasia Gontareva <ana.gontareva@yandex.ru>
2021-03-22 14:18:48 +0300

    login, signin, profile

--- static/js/form.js
+++ static/js/form.js
@@ -6,3 +6,11 @@ window.submitSigninForm = function (form) {
 	//eslint-disable-next-line
 	console.log(form);
 };
+window.saveChanges = function (form) {
+	//eslint-disable-next-line
+	console.log(form);
+};
+window.savePassword = function (form) {
+	//eslint-disable-next-line
+	console.log(form);
+};

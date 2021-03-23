8ecf8e55d8dd8ff9d32f7287763156532185c5b1
Anastasia Gontareva <ana.gontareva@yandex.ru>
2021-03-24 00:20:17 +0300

    added getdata function fro forms

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const scss = require('scss');
 
 const PORT = 3000;
 
 const app = express();
 
-app.use('/static', express.static('./static'));
-scss.renderSync({ file: 'public/main.scss' });
+app.use(express.static('./dist'));
 
 app.listen(PORT, 'localhost', () => {
 	//eslint-disable-next-line
--- static/js/form.js
+++ static/js/form.js
@@ -1,16 +1,32 @@
 window.submitLoginForm = function (form) {
 	//eslint-disable-next-line
-	console.log(form);
+	console.log(getData(form));
 };
 window.submitSigninForm = function (form) {
 	//eslint-disable-next-line
-	console.log(form);
+	console.log(getData(form));
 };
 window.saveChanges = function (form) {
 	//eslint-disable-next-line
-	console.log(form);
+	console.log(getData(form));
 };
 window.savePassword = function (form) {
 	//eslint-disable-next-line
-	console.log(form);
+	console.log(getData(form));
 };
+
+function getData(form) {
+	const data = {};
+
+	if (form && form.hasChildNodes()) {
+		form.childNodes.forEach((node) => {
+			if (node.hasChildNodes()) {
+				Object.assign(data, getData(node));
+			} else if (node.nodeName === 'INPUT') {
+				data[node.name] = node.value;
+			}
+		});
+	}
+
+	return data;
+}
